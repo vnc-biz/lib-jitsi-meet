928c698f28529accd6de5952b4ac81d20402489b
IgorVNC <ihor.khomenko@vnc.biz>
2020-04-24 17:02:54 +0300

    removed 256 bitrate cap

--- modules/xmpp/JingleSessionPC.js
+++ modules/xmpp/JingleSessionPC.js
@@ -1621,7 +1621,7 @@ export default class JingleSessionPC extends JingleSession {
         });
         remoteSdp.raw = remoteSdp.session + remoteSdp.media.join('');
 
-        remoteSdp.raw = remoteSdp.raw.replace('a=rtpmap:100 VP8/90000', 'a=rtpmap:100 VP8/90000\r\na=fmtp:100 x-google-max-bitrate=256');
+        // remoteSdp.raw = remoteSdp.raw.replace('a=rtpmap:100 VP8/90000', 'a=rtpmap:100 VP8/90000\r\na=fmtp:100 x-google-max-bitrate=256');
 
         return remoteSdp;
     }
@@ -1641,7 +1641,7 @@ export default class JingleSessionPC extends JingleSession {
         });
         remoteSdp.raw = remoteSdp.session + remoteSdp.media.join('');
 
-        remoteSdp.raw = remoteSdp.raw.replace('a=rtpmap:100 VP8/90000', 'a=rtpmap:100 VP8/90000\r\na=fmtp:100 x-google-max-bitrate=256');
+        // remoteSdp.raw = remoteSdp.raw.replace('a=rtpmap:100 VP8/90000', 'a=rtpmap:100 VP8/90000\r\na=fmtp:100 x-google-max-bitrate=256');
 
         return remoteSdp;
     }
